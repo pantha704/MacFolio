d9224a98f9e6db442c543681bb0dcef89499a28c
Pratham Jaiswal <100998543+pantha704@users.noreply.github.com>
2026-09-07 12:19:09 +0530

    chore: remove generated declarations from source tree

--- src/components/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Navbar from "./Navbar";
-import Welcome from "./Welcome";
-import Dock from "./Dock";
-import WindowControls from "./WindowControls";
-import NoInternet from "./NoInternet";
-export { Navbar, Welcome, Dock, WindowControls, NoInternet };
