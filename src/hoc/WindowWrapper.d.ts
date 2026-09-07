30d8cdd80dcab8b59613b25b83f95d28187bffc8
Pratham Jaiswal <100998543+pantha704@users.noreply.github.com>
2026-09-07 12:19:42 +0530

    chore: remove generated declarations from source tree

--- src/hoc/WindowWrapper.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { WindowKey } from '#store/useWindowStore';
-declare const WindowWrapper: (Component: React.ComponentType<Record<string, unknown>>, windowKey: WindowKey) => {
-    (props: Record<string, unknown>): import("react/jsx-runtime").JSX.Element | null;
-    displayName: string;
-};
-export default WindowWrapper;
