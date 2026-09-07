2bd548ab113ccb6cce4a5d46064c74da274af0d7
Pratham Jaiswal <100998543+pantha704@users.noreply.github.com>
2026-09-07 12:04:56 +0530

    perf: stop booting WebContainer for every visitor

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { WebContainerProvider } from '#context/WebContainerContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <WebContainerProvider>
-      <App />
-    </WebContainerProvider>
+    <App />
   </StrictMode>,
 )
