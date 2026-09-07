f8d0c2f3e09056eaa8674314729464a4f43fecf9
Pratham Jaiswal <100998543+pantha704@users.noreply.github.com>
2026-09-07 12:13:52 +0530

    refactor: remove obsolete component export

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,5 @@ import Welcome from "./Welcome";
 import Dock from "./Dock";
 import WindowControls from "./WindowControls";
 
-import NoInternet from "./NoInternet";
 
-export { Navbar, Welcome, Dock, WindowControls, NoInternet };
+export { Navbar, Welcome, Dock, WindowControls };
