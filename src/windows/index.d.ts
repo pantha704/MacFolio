210fe754db86f614c18df603d53be6db5c65e2a4
Pratham Jaiswal <100998543+pantha704@users.noreply.github.com>
2026-09-07 12:20:19 +0530

    chore: remove generated declarations from source tree

--- src/windows/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Terminal from './Terminal';
-import Safari from './Safari';
-import Finder from './Finder';
-import Gallery from './Gallery';
-import Contact from './Contact';
-export { Terminal, Safari, Finder, Gallery, Contact };
